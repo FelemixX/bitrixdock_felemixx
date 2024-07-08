39b3bf619f441a04468ece3c7fb257ae5549707b
Alexander Mischinkov <am21workmail@gmail.com>
2024-07-08 20:10:07 +0300

    Исправил пути в классе

--- xhprof_class.php
+++ xhprof_class.php
@@ -1,9 +1,10 @@
+<?php
 class XHProfiler
 {
     public static function startProfiling()
     {
-        include_once '/usr/local/lib/php/xhprof_lib/utils/xhprof_lib.php';
-        include_once '/usr/local/lib/php/xhprof_lib/utils/xhprof_runs.php';
+        include_once '/xhprof/xhprof_lib/utils/xhprof_lib.php';
+        include_once '/xhprof/xhprof_lib/utils/xhprof_runs.php';
         xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
 //        register_shutdown_function([$this, 'saveProfile']);
     }
